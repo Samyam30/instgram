552c5cd7c80fd2c26bdf8474e6667c62a69c5f2e
Samyam <samyambudhathokisc@gmail.com>
2024-07-28 14:49:33 +0545

    user list box

--- src/app/dashboard/[webSocket]/layout.tsx
+++ src/app/dashboard/[webSocket]/layout.tsx
@@ -36,22 +36,22 @@ export default function AuthLayout({
     <div className="page flex flex-row flex-wrap">
       <div className=" h-screen w-1/4  ">
         <h1 className="ml-[22px] font-bold text-[20px]">Your friends: </h1>
-        {data.map((item) => {
-          return (
-            <div
-              className="flex flex-row gap-1 border-[1.5px]  border-solid border-black rounded-lg min-w-[150px] w-[auto] mr-[20px] ml-[20px] mt-[30px] px-[10px] pt-[13px] h-[60px]  "
-              key={item.id}
-              id="friends"
-              onClick={() => {
-                chat(item.email);
-              }}
-            >
-              <p>
+        <div className="flex flex-col gap-3">
+          {data.map((item) => {
+            return (
+              <div
+                className=" border-[1.5px]  border-solid border-black rounded-lg min-w-[280px] mr-[20px] ml-[20px] mt-[30px] px-[10px] pt-[12px] h-[60px]  "
+                key={item.id}
+                id="friends"
+                onClick={() => {
+                  chat(item.email);
+                }}
+              >
                 {item.id}: {item.email}
-              </p>
-            </div>
-          );
-        })}
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className=" w-3/4">{children}</div>
     </div>
